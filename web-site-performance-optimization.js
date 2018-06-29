8fe11c8b42920cf834bf80e9b2d0cd4dd7b13c74
si180@yahoo.com <si180@yahoo.com>
2018-06-28 22:45:46 -0400

    javascript snippet

---
+++ web-site-performance-optimization.js
@@ -0,0 +1,23 @@
+1. Image Optimization
+2. Reduce HTTP Requests
+3. Minify CSS and Javascript
+4. Critical Path and Render Blocking Resources (CSS + JS)
+5. Reduce Latency with a CDN
+6. Time to First Byte (TTFB)
+7. Avoid 301 Redirects
+8. Caching
+9. Prefetch and Preconnect
+10. HTTP/2
+11. Web Font Performance
+12. Hotlink protection
+13. Enable Gzip Compression
+14. Infrastructure
+15. Fix 404 Errors
+16. Serve Scaled Images
+17. Database Optimization
+18. Brosers compatability:  http://browsershots.org/
+19. https://www.keycdn.com/blog/browser-compatibility-testing-tools/
+20. Movile view
+21. Lighthouse, an automated tool and a part of Chrome Developer Tools
+22. https://developers.google.com/web/tools/chrome-user-experience-report/
+23. Analyze and optimize your website with PageSpeed tools: https://developers.google.com/speed/?hl=en-US&utm_source=PSI&utm_medium=incoming-link&utm_campaign=PSI
