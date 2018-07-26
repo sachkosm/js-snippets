28dfee903739d29ca5196020c39b9ea7bce92327
si180@yahoo.com <si180@yahoo.com>
2018-07-26 14:37:28 -0400

    javascript snippet

---
+++ generator-with-promise.js
@@ -0,0 +1,29 @@
+//Async is executed and then the result of the async is provided to the next yield
+//Next yield is called in the ".then" of the previouse yield(promise)
+
+
+function* genFunc() {
+  var data = 0;
+  yield new Promise((resolve, reject) => setTimeout(function() {
+    data = 17;
+    resolve(25);
+  }, 3000)) //async)
+
+  yield data; //data has been modified by the previouse yield
+}
+
+var it = genFunc();
+
+var promise = it.next().value //returns a Promise
+
+console.log(promise);
+
+promise.then(data => {
+  console.log(1);
+  console.log(it.next().value)
+
+  console.log(data)
+  //we could exceute another async and then call next yield
+  //setTimeout(() => console.log( it.next().value ), 2000);
+
+})
