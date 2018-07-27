11b0b4f2fc9bebef1e5ae9f024d3d3eb8e608018
si180@yahoo.com <si180@yahoo.com>
2018-07-27 15:00:43 -0400

    javascript snippet

--- forEach-map-reduce-filter.js
+++ forEach-map-reduce-filter.js
@@ -6,10 +6,9 @@ arr.forEach(function(currentValue, currentIndex, arr) {
 }, this);
 
 
-
 //Returns
 //A new array with each element being the result of the callback function.
-var new_array = arr.map(function(currentValue, currentIndex, arr) {
+var newArray = arr.map(function(currentValue, currentIndex, arr) {
     // Return element for new_array
 }, this)
 
@@ -20,3 +19,10 @@ var new_array = arr.map(function(currentValue, currentIndex, arr) {
 var result = arr.reduce(function(accumulator, currentValue, currentIndex, arr) {
   //return accumulator + some logic;
 }), initialValue);
+
+
+//Returns
+//A new array with each element being the result of the callback function.
+var newArray = arr.filter(function(currentValue, index, arr){
+  //return currentValue -> if pass the test here
+}, thisArg)
