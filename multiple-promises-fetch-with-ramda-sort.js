6a7fff92d12804f9a6a48b666004d1060a8e9105
si180@yahoo.com <si180@yahoo.com>
2018-07-28 17:14:47 -0400

    javascript snippet

--- multiple-promises-fetch-with-ramda-sort.js
+++ multiple-promises-fetch-with-ramda-sort.js
@@ -2,27 +2,27 @@ const fetch = require("node-fetch"); // Tested on Node side
 const R = require("ramda");
 //Setup Ramda help functions
 const sortByProp = (propName) => R.sortBy(R.prop(propName));
-const sortBtPropDesc = (propName) => R.compose(R.reverse, R.sortBy(R.prop(propName)));
+const sortByPropDesc = (propName) => R.compose(R.reverse, R.sortBy(R.prop(propName)));
 
-const combinedArr = [];
+const arrCombinedResult = [];
 const urls = Array(15);
-urls.fill('https://randomuser.me/api/')
+urls.fill('https://randomuser.me/api/') // using user random emulation
 
 
-const grabContent = url => fetch(url)
+const fetchOneUrl = url => fetch(url)
   .then(function(response) {
     var s = response.json(); // json retuns a promise, use then to read the result
     return s;
   })
   .then(function(myJson) {
     //const sortedData = sortByProp('gender')(myJson.results) //If we need inside sort
-    combinedArr.push(myJson.results[0]) //[0] to make this array of object - by default iti is array of array of object
+    arrCombinedResult.push(myJson.results[0]) //[0] to make this array of objects - by default iti is array of array of object
     //console.log(sortedData);
   })
 
 Promise
-  .all(urls.map(grabContent))
-  .then((data) => {
-    const sortedData = sortBtPropDesc('gender')(combinedArr);
+  .all(urls.map(fetchOneUrl))
+  .then(() => {
+    const sortedData = sortByPropDesc('gender')(arrCombinedResult);
     console.log(sortedData);
   })
