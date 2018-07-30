5f420f7dfa90bd2b26fedd34799280f6a6731583
si180@yahoo.com <si180@yahoo.com>
2018-07-29 22:44:23 -0400

    javascript snippet

---
+++ return-function.js
@@ -0,0 +1,12 @@
+
+
+
+const users = ['a', 'b', 'c']
+
+const matchName = (name) => (item) => item === name; //Function returning a function
+
+const curriedFunc = matchName('a') //Seed the retruning function with the first parameter - name
+
+const result = users.find(curriedFunc) //curriedFunc will be called fro each element like this : curriedFunc(item, index, arr)
+
+console.log(result)
