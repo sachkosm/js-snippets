b55a63264d49f0ead3a9d8b0ca9d5587596380f7
si180@yahoo.com <si180@yahoo.com>
2018-07-30 21:32:41 -0400

    javascript snippet

--- return-function-currying.js
+++ return-function-currying.js
@@ -1,12 +1,24 @@
-
+// In users.find(curriedFunc)
+// Find wwill call our function as follow:
+//curriedFunc(item, index, arr) -> but we need to add additional parameter 'name'
 
 const users = ['a', 'b', 'c']
 
-const matchName = (name) => (item) => item === name; //Function returning a function
+//1. Create Function that returs a function and take parameter "name"
+const matchName = (name) => (item) => item === name;
 
-const curriedFunc = matchName('a') //Seed the returning function with the first parameter - name
+//Seed the returning function with the first parameter - name
+const curriedFunc = matchName('a') 
 
-const result = users.find(curriedFunc) //curriedFunc will be called fro each element like this : curriedFunc(item, index, arr)
+//curriedFunc will be called for each element like this : curriedFunc(item, index, arr)
+// but the function already has closure on 'a'
+const result = users.find(curriedFunc) 
 
 console.log(result)
+
+//We can also use it like this directly
+const result2 = users.find(matchName('b'))
+// It wil be called like this: users.find(matchName('b')(item, index, arr))
+
+console.log(result2)
 
