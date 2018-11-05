27c26463246e8faf9d787d397b79ebca42456b99
sachkosm <39930413+sachkosm@users.noreply.github.com>
2018-11-05 16:40:48 -0500

    Create resizable-lib.js

---
+++ resizable-lib.js
@@ -0,0 +1,160 @@
+
+export const znResizeable = function (resizer, drawer, report, config) {
+    //Small library to make the ReportDrawer resizable
+
+    if (!resizer) { console.error('Resizer element is not provided!'); return }
+    if (!drawer) { console.error('Drawer element is not provided!'); return }
+
+    var drawerStartWidth;
+    var newDrawerWidth;
+    var reportStartWidth;
+    var newReportWidth;
+    var resizerWidth = 9
+    var borderDrawerToReport = 0
+    var minSizeLimit = 200
+    var maxSizeLimit = 1000
+    var initialized = false
+
+    if (config) {
+        resizerWidth = config.resizerWidth ? config.resizerWidth : resizerWidth
+        borderDrawerToReport = config.borderDrawerToReport ? config.borderDrawerToReport : borderDrawerToReport
+        minSizeLimit = config.minSizeLimit ? config.minSizeLimit : minSizeLimit
+        maxSizeLimit = config.maxSizeLimit ? config.maxSizeLimit : maxSizeLimit
+    }
+
+    let gripStyle = `flex: 0 0 auto;
+        width: ` + resizerWidth + `px;
+        float: right;
+        cursor: col-resize;
+        height: 100%;
+        background: url(public_libs/resources/vsizegrip.png) no-repeat center center #ffffff;
+        min-height: 200px
+    `
+
+    function currentWidth() {
+        return { 'drawerStartWidth': drawerStartWidth, 'newDrawerWidth': newDrawerWidth, 'reportStartWidth': reportStartWidth, 'newReportWidth': newReportWidth }
+    }
+
+    function getDrawerStartWidth() {
+        return drawerStartWidth
+    }
+
+    function setReport(newReport) {
+        report = newReport
+        initResize()
+    }
+
+    function setNewReportWidthBasedOnDrawer() {
+        let bodyWidth = document.getElementsByTagName('body')[0].clientWidth || console.error('Can not get element body!')
+        let drawerCurrenttWidth = parseInt(document.defaultView.getComputedStyle(drawer).width, 10) || console.error('Can not get drawer element!')
+        let reportComputedSetWidth = (bodyWidth - (drawerCurrenttWidth + borderDrawerToReport))
+        report ? report.style.width = reportComputedSetWidth + 'px' : null
+    }
+
+    function setNewDrawerWidth() {
+        drawerStartWidth = newDrawerWidth
+        drawer.style.width = newDrawerWidth
+    }
+
+    function initResize() {
+        //If the resiable has been already initilized
+        if (initialized) {
+            setNewDrawerWidth()
+            setNewReportWidthBasedOnDrawer()
+        }
+        else {
+            //Initialization of the initial width - should happen only when the component is created
+            newDrawerWidth = drawerStartWidth = parseInt(document.defaultView.getComputedStyle(drawer).width, 10) || console.error('Can not get drawer element!')
+            setNewReportWidthBasedOnDrawer()
+            initialized = true
+        }
+
+        if (report) {
+            //If there is a style already - we need to add the new one to the existing one
+            let st = report.getAttribute("style")
+            if (st) {
+                report.setAttribute("style", st + "float: right;")
+            }
+            else {
+                report.setAttribute("style", "float: right;")
+            }
+        }
+
+        //Init draging 
+        function initDrag(e) {
+            e.preventDefault();
+            window.startX = e.clientX;
+            drawerStartWidth = parseInt(document.defaultView.getComputedStyle(drawer).width, 10);
+            report ? reportStartWidth = parseInt(document.defaultView.getComputedStyle(report).width, 10) : 0;
+            document.documentElement.addEventListener('mousemove', doDrag, false);
+            document.documentElement.addEventListener('mouseup', stopDrag, false);
+        }
+
+        //calculate and apply new width according to the mouse movement
+        function doDrag(e) {
+            e.preventDefault();
+            let pxMoved = e.clientX - window.startX
+            if ((drawerStartWidth + pxMoved) > minSizeLimit && (drawerStartWidth + pxMoved) < maxSizeLimit) {
+                newDrawerWidth = (drawerStartWidth + pxMoved);
+                newReportWidth = (reportStartWidth - pxMoved);
+                drawer.style.width = newDrawerWidth + 'px'
+                if (report) {
+                    report.style.width = newReportWidth + 'px'
+                }
+            }
+        }
+        //Remove the events that track the mouse movements and update golden layout
+        function stopDrag(e) {
+            document.documentElement.removeEventListener('mousemove', doDrag, false);
+            document.documentElement.removeEventListener('mouseup', stopDrag, false);
+            setTimeout(() => {
+                window.dispatchEvent(new Event('resize'));
+            }, 0)
+        }
+
+        //Init
+        function init() {
+            if (report) {
+                if (!report.hasAttribute("class", "resizable")) {
+                    report.className = report.className + ' resizable';
+                }
+            }
+            if (!resizer.hasAttribute("class", "resizable")) {
+                resizer.setAttribute("style", gripStyle)
+                resizer.className = resizer.className + ' resizable';
+            }
+            resizer.removeEventListener('mousedown', initDrag, false);
+            resizer.addEventListener('mousedown', initDrag, false);
+        }
+
+        //Initialize
+        init()
+    }
+
+    function removeResizing() {
+        resizer.removeEventListener('mousedown', initResize.initDrag, false);
+        report ? report.removeAttribute("style", "float: right") : null
+        resizer.removeAttribute("style", gripStyle)
+        initialized = false
+    }
+
+    function isInitialized() {
+        return initialized
+    }
+
+    function reportIsSet() {
+        return report ? true : false
+    }
+
+    return {
+        initResize: initResize,
+        removeResizing: removeResizing,
+        setReport: setReport,
+        getDrawerStartWidth: getDrawerStartWidth,
+        currentWidth: currentWidth,
+        setNewReportWidthBasedOnDrawer: setNewReportWidthBasedOnDrawer,
+        isInitialized: isInitialized,
+        reportIsSet: reportIsSet
+    }
+
+}
