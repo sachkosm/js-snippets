957bfeac368cf927f3dee8409215b7e5b57c7837
sachkosm <39930413+sachkosm@users.noreply.github.com>
2018-12-05 11:51:31 -0500

    Update remote-positions.js

--- remote-positions.js
+++ remote-positions.js
@@ -1,1 +1,3 @@
 https://stackoverflow.com/jobs/remote-developer-jobs?med=clc
+https://stackoverflow.com/jobs/146458/senior-react-engineer-100-remote-north-citrusbyte?med=clc
+https://stackoverflow.com/jobs/207685/software-engineer-full-stack-front-end-focused-motiion?med=clc
