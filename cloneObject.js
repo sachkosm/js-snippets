43ed3b843447d7446a6201939cfc8ca3c5defb4a
sachkosm <39930413+sachkosm@users.noreply.github.com>
2018-12-18 10:42:12 -0500

    Update cloneObject.js

--- cloneObject.js
+++ cloneObject.js
@@ -1,4 +1,9 @@
+=== Deep cloning and making sure only plain objects in string format are copied =====
+            
+ let objectCopy = JSON.parse(JSON.stringify(object))
 
+
+=====================================
 const cloneObject = function (object) {
             var copy = {};
             var keys = Object.keys(object);
@@ -12,44 +17,54 @@ const cloneObject = function (object) {
 
 ===============================================
 
-function clone(/*anything*/ src) {
-    // summary:
-    //		Clones objects (including DOM nodes) and all children.
-    //		Warning: do not clone cyclic structures.
-    // src:
-    //		The object to clone
-    if (!src || typeof src != "object" || lang.isFunction(src)) {
-        // null, undefined, any non-object, or function
-        return src;	// anything
-    }
-    if (src.nodeType && "cloneNode" in src) {
-        // DOM Node
-        return src.cloneNode(true); // Node
-    }
-    if (src instanceof Date) {
-        // Date
-        return new Date(src.getTime());	// Date
-    }
-    if (src instanceof RegExp) {
-        // RegExp
-        return new RegExp(src);   // RegExp
-    }
-    var r, i, l;
-    if (lang.isArray(src)) {
-        // array
-        r = [];
-        for (i = 0, l = src.length; i < l; ++i) {
-            if (i in src) {
-                r[i] = lang.clone(src[i]);
-            }
-        }
-        // we don't clone functions for performance reasons
-        //		}else if(d.isFunction(src)){
-        //			// function
-        //			r = function(){ return src.apply(this, arguments); };
-    } else {
-        // generic objects
-        r = src.constructor ? new src.constructor() : {};
-    }
-    return lang._mixin(r, src, lang.clone);
+function clone(src) {
+	function mixin(dest, source, copyFunc) {
+		var name, s, i, empty = {};
+		for (name in source) {
+			// the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+			// inherited from Object.prototype.	 For example, if dest has a custom toString() method,
+			// don't overwrite it with the toString() method that source inherited from Object.prototype
+			s = source[name];
+			if (!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))) {
+				dest[name] = copyFunc ? copyFunc(s) : s;
+			}
+		}
+		return dest;
+	}
+
+	if (!src || typeof src != "object" || Object.prototype.toString.call(src) === "[object Function]") {
+		// null, undefined, any non-object, or function
+		return src; // anything
+	}
+	if (src.nodeType && "cloneNode" in src) {
+		// DOM Node
+		return src.cloneNode(true); // Node
+	}
+	if (src instanceof Date) {
+		// Date
+		return new Date(src.getTime()); // Date
+	}
+	if (src instanceof RegExp) {
+		// RegExp
+		return new RegExp(src); // RegExp
+	}
+	var r, i, l;
+	if (src instanceof Array) {
+		// array
+		r = [];
+		for (i = 0, l = src.length; i < l; ++i) {
+			if (i in src) {
+				r.push(clone(src[i]));
+			}
+		}
+		// we don't clone functions for performance reasons
+		//		}else if(d.isFunction(src)){
+		//			// function
+		//			r = function(){ return src.apply(this, arguments); };
+	} else {
+		// generic objects
+		r = src.constructor ? new src.constructor() : {};
+	}
+	return mixin(r, src, clone);
+
 }
