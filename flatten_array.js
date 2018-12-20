ed5bd30b5dcffc7b955c19e4b27e2f7cc962d01a
sachkosm <39930413+sachkosm@users.noreply.github.com>
2018-12-19 23:49:53 -0500

    Create flatten_array.js

---
+++ flatten_array.js
@@ -0,0 +1,19 @@
+//non recursive flatten deep using a stack
+
+var arr1 = [1,2,3,[1,2,3,4, [2,3,4]]];
+function flatten(input) {
+  const stack = [...input];
+  const res = [];
+  while (stack.length) {
+    // pop value from stack
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      // push back array items, won't modify the original input
+      stack.push(...next);
+    } else {
+      res.push(next);
+    }
+  }
+  //reverse to restore input order
+  return res.reverse();
+}
