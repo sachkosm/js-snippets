1052600219a407c387e0ebe482f6d3154caf05ca
sachkosm <39930413+sachkosm@users.noreply.github.com>
2018-12-19 23:50:53 -0500

    Create flatten_array_reduce.js

---
+++ flatten_array_reduce.js
@@ -0,0 +1,7 @@
+//to enable deep level flatten use recursion with reduce and concat
+var arr1 = [1,2,3,[1,2,3,4, [2,3,4]]];
+
+function flattenDeep(arr1) {
+   return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
+flattenDeep(arr1);// [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
