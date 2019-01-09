2a2139b19d7d9ed232b9b7dca0e36db6b2e2d9f4
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-01-09 12:30:06 -0500

    React.lazy, React.Suspense
    React.Suspense let you specify the loading indicator in case some components in the tree below it are not yet ready to render. Today, lazy loading components is the only use case supported by <React.Suspense>

---
+++ react-Loading-Indicator.js
@@ -0,0 +1,13 @@
+// This component is loaded dynamically
+const OtherComponent = React.lazy(() => import('./OtherComponent'));
+
+function MyComponent() {
+  return (
+    // Displays <Spinner> until OtherComponent loads
+    <React.Suspense fallback={<Spinner />}>
+      <div>
+        <OtherComponent />
+      </div>
+    </React.Suspense>
+  );
+}
