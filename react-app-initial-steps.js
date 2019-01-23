e6c46d5488da0bb613114c35863ca230e555907f
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-01-23 09:43:22 -0500

    Create react-app-initial-steps.js

---
+++ react-app-initial-steps.js
@@ -0,0 +1,8 @@
+
+npx create-react-app my-app
+
+npm install --save react-router-dom
+
+https://facebook.github.io/create-react-app/docs/getting-started
+
+https://reacttraining.com/react-router/web/example/basic
