8dfa4713bd1836d78795ee618d4efedd16b70993
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-01-24 11:45:47 -0500

    Create interview-considerations.js

---
+++ interview-considerations.js
@@ -0,0 +1,26 @@
+
+1. Who is interviewing  you
+  	- race
+  	- age
+  	- personal character
+  	- experience
+  	- knowledge
+
+2. The actual current need of the company/manager has at the moment
+	- current or potential projects / contract
+	- deadlines
+	- recently positions vacancy
+	- budget
+	- recently approved new positions
+
+3. The availability of professionals at the moment for this company and particular position
+	- current market situation and state of the economy 
+	- the place of the company
+	- the compensation and benefits and requirements
+
+4. The match between the need and the person
+	- technology
+	- experience
+	- character
+
+5. Everything else
