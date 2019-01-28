6d95542abd3e1780649dcac34f9ab9a758057e45
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-01-28 10:35:23 -0500

    Update redux-flow.js

--- redux-flow.js
+++ redux-flow.js
@@ -12,7 +12,7 @@ Reac-Redux
 
 3. store.dispatch - will execute:
     - currentReducer ( combined reducers )
-    - execute all subscribed listeners by executing the Provider's function to chnage it's state
+    - execute all subscribed listeners by executing the Provider's function to change it's state
     - will return back whathever it was passed as parameter ( it is object or function or promiss )
     
 4. Changing the state of the Provider will pass all props to it's children components
@@ -21,6 +21,8 @@ Reac-Redux
 
 6. Depending on if the new props used by a component are diferent or not  - the conponents will be rerendered or not 
   if they have been connected with connect
+  
+7. Connect wraps the compnents and provides the props from the redux store
   
   
 
