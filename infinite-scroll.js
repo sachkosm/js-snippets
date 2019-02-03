ca48531063b7b502993d23987226827826a71706
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-02-02 21:49:05 -0500

    Update infinite-scroll.js

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -529,3 +529,39 @@
         return e ? this.bind("smartscroll", e) : this.trigger("smartscroll", ["execAsap"])
     }
 });
+
+
+
+//============ Usage ======================
+
+$(document).ready(function() {
+	var grid = $('.grid');
+
+	grid.masonry({
+		itemSelector: '.grid-item',
+		columnWidth: 200
+	});
+
+	grid.infinitescroll({
+		// Pagination element that will be hidden
+		navSelector: '#pagination',
+
+		// Next page link
+		nextSelector: '#pagination p a',
+
+		// Selector of items to retrieve
+		itemSelector: '.grid-item',
+
+		// Loading message
+		loadingText: 'Loading new items…'
+	},
+
+	// Function called once the elements are retrieved
+	function(new_elts) {
+		var elts = $(new_elts).css('opacity', 0);
+
+		elts.animate({opacity: 1});
+		grid.masonry('appended', elts);
+	});
+});
+
