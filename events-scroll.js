5076a2d8b572ac2a1737bd093819e3da4c0746bd
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-02-05 09:37:38 -0500

    Create events-scroll.js

---
+++ events-scroll.js
@@ -0,0 +1,10 @@
+        domElement.addEventListener('scroll', (event) => {
+            //console.warn('scroll event', event)
+            // console.warn(' SCROLLING')
+            // console.warn('domElement scrollHeight', domElement.scrollHeight)
+            // console.warn('domElement clientHeight', domElement.clientHeight)
+            // console.warn('domElement offsetHeight', domElement.offsetHeight)
+            // console.warn('domElement getComputedStyle', window.getComputedStyle(domElement).height)
+            // console.warn('domElement scrollTop', domElement.scrollTop)
+
+        })
