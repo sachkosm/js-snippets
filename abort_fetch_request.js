71f18e60b83cbb6355242034b32ac41d92915932
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-04-16 23:25:08 -0400

    Create abort_fetch_request.js

---
+++ abort_fetch_request.js
@@ -0,0 +1,38 @@
+https://stackoverflow.com/questions/31061838/how-do-i-cancel-an-http-fetch-request
+
+<script>
+    // Create an instance.
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    /*
+    // Register a listenr.
+    signal.addEventListener("abort", () => {
+        console.log("aborted!")
+    })
+    */
+
+
+    function beginFetching() {
+        console.log('Now fetching');
+        var urlToFetch = "https://httpbin.org/delay/3";
+
+        fetch(urlToFetch, {
+                method: 'get',
+                signal: signal,
+            })
+            .then(function(response) {
+                console.log(`Fetch complete. (Not aborted)`);
+            }).catch(function(err) {
+                console.error(` Err: ${err}`);
+            });
+    }
+
+
+    function abortFetching() {
+        console.log('Now aborting');
+        // Abort.
+        controller.abort()
+    }
+
+</script>
