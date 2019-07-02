1b605fd587db9808549614ef0a3c6b39dd674054
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-07-02 14:20:57 -0400

    Create storePasswordAsHash.js

---
+++ storePasswordAsHash.js
@@ -0,0 +1,42 @@
+var crypto = require('crypto');
+
+var userStore = []
+
+function hashPassword(user, pass) {
+
+	return new Promise((resolve, reject) => {
+
+		if (user && pass) {
+
+			crypto.randomBytes(128, function(err, salt) {
+
+				if (err) { throw err; }
+
+				salt = new Buffer(salt)
+					.toString('hex');
+
+				crypto.pbkdf2(pass, salt, 7000, 256, 'sha512', function(err, hash) {
+
+					if (err) { throw err; }
+
+					userStore[user] = {
+						salt: salt,
+						hash: (new Buffer(hash)
+							.toString('hex'))
+					};
+					resolve(userStore)
+				});
+
+			})
+		}
+	})
+}
+
+async function f() {
+	await hashPassword('user', 'veryStrongPassword45')
+		.then((userStore) => {
+			console.log(userStore)
+		})
+}
+
+f()
