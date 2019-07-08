0c860b40eb9e7f89d5a490899dc33fd748565c36
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-07-08 14:31:58 -0400

    Create version.js

---
+++ version.js
@@ -0,0 +1,18 @@
+let commit, version;
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+try {
+  commit = execSync('git rev-parse HEAD', { stdio: [] }).toString();
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.error('Problem getting git commit:', e.message);
+}
+try {
+  version = require('../package.json').version;
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.error('Problem getting bPanel version:', e.message);
+}
+
+fs.writeFileSync('webapp/version.json', JSON.stringify({ commit, version }));
